60a5c8d48bb9028bbaa0e71ab182286324260730
Ashay Bodhane <ashaybodhane@gmail.com>
2026-07-13 21:12:58 +0530

    Add files via upload

---
+++ Loops/Factorial.java
@@ -0,0 +1,18 @@
+package Ghoomi;
+import java.util.Scanner;
+public class Factorial {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. to find Factorial" );
+        int n = sc.nextInt();
+        int fact= 1;
+        for(int i=1;i<=n;i++){
+            //fact=fact*i
+            fact*=i;
+        }
+        System.out.println("Factorial : "+fact);
+
+
+
+    }
+}
