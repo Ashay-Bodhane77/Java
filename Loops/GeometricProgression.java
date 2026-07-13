008e7da4e2aa37942f44fc9e2b1d13f02662b752
Ashay Bodhane <ashaybodhane@gmail.com>
2026-07-13 21:16:27 +0530

    Add files via upload

---
+++ Loops/GeometricProgression.java
@@ -0,0 +1,21 @@
+package Ghoomi;
+
+//import java.util.Scanner;
+import java.util.*;
+public class GeometricProgression {
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     int n=sc.nextInt();
+
+     //Print Geometric Progression = 1,2,4,8
+//     common ratio=2
+
+//        int a=1 ,r=2;
+//        for(int i=1;i<=n;i++){
+//            System.out.println(a);
+//            a*=r;
+//        }
+
+    }
+}
