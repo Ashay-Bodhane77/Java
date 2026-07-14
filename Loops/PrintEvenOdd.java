d7c96c7e7c32c08872dee247821eabfad3bedc97
Ashay Bodhane <ashaybodhane@gmail.com>
2026-07-14 20:47:12 +0530

    Add files via upload

---
+++ Loops/PrintEvenOdd.java
@@ -0,0 +1,42 @@
+package Ghoomi;
+
+public class PrintEvenOdd {
+    static void main(String[] args) {
+
+        // Print Even numbers from 1 to 100
+
+//        for(int i=1;i<100;i++){
+//            if(i%2==0) {
+//                System.out.println(i);
+//            }
+//        }
+
+        //PRINT ODD FROM 1 TO 100
+//
+//       for(int i=1;i<100;i++){
+//            if(i%2 !=0 ) {
+//                System.out.println(i);
+//           }
+//       }
+
+        //PRINT TABLE OF 17
+//        for(int i=17;i<=170;i++){
+//            if(i%17==0){
+//                System.out.println(i+" ");
+//            }
+//        }
+
+        // print odd and divisible by 3 from 1 to 100
+//
+//        for(int i=1;i<100;i++){
+//            if(i%2 !=0 && i%3==0) {
+//                System.out.println(i);
+//            }
+//        }
+
+
+
+
+
+    }
+}
